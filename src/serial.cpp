e681f8a14df235bdc81525b56d035c1340ebacdc
Jordan Taylor <jdtaylor7@gmail.com>
2020-07-13 09:31:50 -0700

    Finish serial class.

--- src/serial.cpp
+++ src/serial.cpp
@@ -1,1 +1,66 @@
 #include "serial.hpp"
+
+SerialDevice::SerialDevice(
+        std::shared_ptr<BoundedBuffer<char>> byte_buffer_) :
+    stream{},
+    byte_buffer{byte_buffer_}
+{
+}
+
+bool SerialDevice::open(const std::string& port_name)
+{
+    try
+    {
+        stream.Open(port_name.c_str());
+    }
+    catch (const LibSerial::OpenFailed&)
+    {
+        std::cerr << "Failed to open serial port: " << port_name << '\n';
+        return false;
+    }
+    return true;
+}
+
+void SerialDevice::config(
+    const LibSerial::BaudRate br,
+    const LibSerial::CharacterSize cs,
+    const LibSerial::FlowControl fc,
+    const LibSerial::Parity p,
+    const LibSerial::StopBits sb
+)
+{
+    stream.SetBaudRate(br);
+    stream.SetCharacterSize(cs);
+    stream.SetFlowControl(fc);
+    stream.SetParity(p);
+    stream.SetStopBits(sb);
+}
+
+bool SerialDevice::is_data_available()
+{
+    return stream.IsDataAvailable();
+}
+
+void SerialDevice::start_reading()
+{
+    keep_reading.store(true);
+
+    while (1)
+    {
+        if (keep_reading.load() && is_data_available())
+        {
+            using namespace std::chrono_literals;
+
+            char recv_byte;
+            stream.get(recv_byte);
+            byte_buffer->force_push(recv_byte);
+
+            std::this_thread::sleep_for(1us);
+        }
+    }
+}
+
+void SerialDevice::stop_reading()
+{
+    keep_reading.store(false);
+}
--- test/test_serial.cpp
+++ test/test_serial.cpp
@@ -2,8 +2,8 @@
 #include <string>
 #include <thread>
 
-// #include "serial.hpp"
-#include <libserial/SerialStream.h>
+#include "bounded_buffer.hpp"
+#include "serial.hpp"
 
 const std::string SERIAL_PORT_1{"/dev/ttyACM0"};
 
@@ -11,37 +11,43 @@ int main()
 {
     using namespace LibSerial;
 
-    SerialStream serial_device;
+    /*
+     * Pass buffer to serial device.
+     */
+    auto buf = std::make_shared<BoundedBuffer<char>>(1024);
+    SerialDevice dev{buf};
 
-    try
-    {
-        serial_device.Open(SERIAL_PORT_1.c_str());
-    }
-    catch (const OpenFailed&)
-    {
-        std::cerr << "Failed to open serial port\n";
-        return 1;
-    }
+    /*
+     * Open device.
+     */
+    if (!dev.open(SERIAL_PORT_1)) { return 1; }
 
-    serial_device.SetBaudRate(BaudRate::BAUD_9600) ;
-    serial_device.SetCharacterSize(CharacterSize::CHAR_SIZE_8) ;
-    serial_device.SetFlowControl(FlowControl::FLOW_CONTROL_NONE) ;
-    serial_device.SetParity(Parity::PARITY_NONE) ;
-    serial_device.SetStopBits(StopBits::STOP_BITS_1) ;
+    /*
+     * Configure device.
+     */
+    dev.config(
+        BaudRate::BAUD_9600,
+        CharacterSize::CHAR_SIZE_8,
+        FlowControl::FLOW_CONTROL_NONE,
+        Parity::PARITY_NONE,
+        StopBits::STOP_BITS_1
+    );
 
+    /*
+     * Read data from device into buffer.
+     */
+    std::thread t1([&](){
+        dev.start_reading();
+    });
+
+    /*
+     * Process data from buffer.
+     */
     while (1)
     {
-        if (serial_device.IsDataAvailable())
-        {
-            using namespace std::chrono_literals;
-
-            char recv_byte;
-
-            serial_device.get(recv_byte);
-            std::cout << recv_byte;
-
-            std::this_thread::sleep_for(1us);
-        }
+        auto recv_byte_p = buf->try_pop();
+        if (recv_byte_p)
+            std::cout << *recv_byte_p;
     }
 
     return 0;
